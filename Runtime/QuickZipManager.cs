07b32a69f8a462e770a3b2563f566154e45e883e
Ramon <ramon.oliva.martinez@gmail.com>
2022-12-16 19:29:34 +0100

    * Introduced new methods from zipping/unzipping from byte arrays. This way we avoid the need of an intermediate file.

--- Runtime/QuickZipManager.cs
+++ Runtime/QuickZipManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using System.IO;
 
 using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Core;
 
 namespace QuickVR
 {
@@ -30,7 +31,7 @@ namespace QuickVR
 
         #endregion
 
-        #region CREATION AND DESTRUCTION
+        #region GET AND SET
 
         /// <summary>
         /// Creates a zip file. 
@@ -47,6 +48,69 @@ namespace QuickVR
             {
                 CreateZipFromDirectory(pathSrc, pathDst);
             }
+        }
+
+        /// <summary>
+        /// Creates a zip file from an array of bytes. 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="entryName"></param>
+        /// <param name="pathDst"></param>
+        public static void CreateZip(byte[] data, string entryName, string pathDst)
+        {
+            byte[] zipData = CreateZip(data, entryName);
+
+            BinaryWriter bWriter = new BinaryWriter(File.OpenWrite(pathDst));
+            bWriter.Write(zipData);
+            bWriter.Close();
+        }
+
+        /// <summary>
+        /// Creates a zip byte array from the input data. 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="entryName"></param>
+        /// <returns></returns>
+        public static byte[] CreateZip(byte[] data, string entryName)
+        {
+            MemoryStream result = new MemoryStream();
+            ZipOutputStream zipStream = new ZipOutputStream(result);
+            
+            zipStream.SetLevel(3);
+
+            ZipEntry newEntry = new ZipEntry(entryName);
+            newEntry.DateTime = System.DateTime.Now;
+
+            zipStream.PutNextEntry(newEntry);
+
+            StreamUtils.Copy(new MemoryStream(data), zipStream, new byte[4096]);
+            zipStream.CloseEntry();
+            
+            zipStream.IsStreamOwner = false;
+            zipStream.Close();
+
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Extracts the contents of a zip file. 
+        /// </summary>
+        /// <param name="zipPath"></param>
+        public static void ExtractZip(string zipPath, string pathDst)
+        {
+            _zipManager.ExtractZip(zipPath, pathDst, null);
+        }
+
+        public static byte[] ExtractZip(byte[] data)
+        {
+            MemoryStream result = new MemoryStream();
+            MemoryStream inputStream = new MemoryStream(data);
+            ZipInputStream zipStream = new ZipInputStream(inputStream);
+
+            zipStream.GetNextEntry();
+            zipStream.CopyTo(result);
+
+            return result.ToArray();
         }
         
         /// <summary>
@@ -81,10 +145,6 @@ namespace QuickVR
         {
             _zipManager.CreateZip(pathDst, pathSrc, true, null);
         }
-
-        #endregion
-
-        #region GET AND SET
 
         private static bool IsFilePath(string path)
         {
